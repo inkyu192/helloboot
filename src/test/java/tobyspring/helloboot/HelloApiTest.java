8a686f2048139b385971fa36ed3f932725909368
inkyu192 <inkyu192@gmail.com>
2023-04-08 17:32:25 +0900

    DI와 단위 테스트

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -1,6 +1,5 @@
 package tobyspring.helloboot;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpHeaders;
@@ -21,5 +20,14 @@ public class HelloApiTest {
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         assertThat(res.getBody()).isEqualTo("Hello Spring");
+    }
+
+    @Test
+    void failHelloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
