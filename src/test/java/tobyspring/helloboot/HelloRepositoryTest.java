1ecf6a796ccc8baabc80266f6a1b2999c58a1f4c
inkyu192 <inkyu192@gmail.com>
2023-04-21 00:38:39 +0900

    리포지토리를 사용하는 HelloService

--- src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -1,12 +1,10 @@
 package tobyspring.helloboot;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @HelloBootTest
 public class HelloRepositoryTest {
@@ -15,11 +13,6 @@ public class HelloRepositoryTest {
 
     @Autowired
     HelloRepository helloRepository;
-
-    @BeforeEach
-    void init() {
-        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-    }
 
     @Test
     void findHelloFailed() {
--- src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
+++ src/test/java/tobyspring/helloboot/JdbcTemplateTest.java
@@ -1,22 +1,15 @@
 package tobyspring.helloboot;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @HelloBootTest
 public class JdbcTemplateTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
-
-    @BeforeEach
-    void init() {
-        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-    }
 
     @Test
     void insertAndQuery() {
